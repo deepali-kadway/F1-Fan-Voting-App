619c30f2d0846ed7e6a8f90f26cd31149e46860c
Deepali Kadway <dkadway@gmail.com>
2025-06-11 22:49:09 -0600

    Added code for POST

--- routes/drivers.js
+++ routes/drivers.js
@@ -2,11 +2,28 @@ const express = require('express');
 const schema = require('../models/driverschema')
 const router = express.Router()
 
-//Fetch all drivers from the database
+//Fetch all f1 drivers from the database
 router.get('/', async (req, res) => {
  // res.send('Fetching all drivers');  
     const driver = await schema.find()
     res.json(driver)
 })
 
+//Add a new f1 driver to the database. Admin portal usually can do this.
+router.post('/', async (req, res) => {
+    const newDriver = new schema({
+        name: req.body.name,
+        team: req.body.team,
+        country: req.body.country,
+        podiums: req.body.podiums,
+        image: req.body.image
+    })
+
+    try {
+        const savedDriver = await newDriver.save()
+        res.status(201).json(savedDriver)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
 module.exports = router
