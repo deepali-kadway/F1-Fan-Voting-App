e592587b41dd846260b9c95a606a31405389e992
Deepali Kadway <dkadway@gmail.com>
2025-06-13 19:03:39 -0600

    Updated logic for PUT method to add votes

--- routes/drivers.js
+++ routes/drivers.js
@@ -11,12 +11,13 @@ router.get('/', async (req, res) => {
 
 //Add a new f1 driver to the database. Admin portal usually can do this.
 router.post('/', async (req, res) => {
+
     const newDriver = new schema({
         name: req.body.name,
         team: req.body.team,
         country: req.body.country,
         podiums: req.body.podiums,
-        image: req.body.image
+        image: req.body.image,
     })
 
     try {
@@ -26,4 +27,22 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 })
+
+//Update the vote count for a driver
+router.put('/:id/votes', async (req, res) => {
+    try{
+        const updateVote = await schema.findByIdAndUpdate(req.params.id)
+        if(!updateVote){
+            return res.status(404).json({message: 'Driver not found'})
+        }
+        updateVote.votes += 1
+        await updateVote.save()
+        res.json(updateVote)
+        res.status(200).json(updateVote)
+    }
+    catch (error){
+        res.status(500).json({ message: error.message})
+    }
+})
+
 module.exports = router
