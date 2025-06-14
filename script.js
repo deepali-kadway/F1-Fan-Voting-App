e8eb115d0c9edbd04d65344d2424cb051626d34f
Deepali Kadway <dkadway@gmail.com>
2025-06-13 19:03:58 -0600

    Add Vote functionality added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+    fetchDrivers()
+})
+
+function fetchDrivers() {
     fetch('http://localhost:3002/drivers')
     .then(res => res.json())
     .then(drivers => {
+        console.log(drivers); 
         const container = document.getElementById('candidates')
         container.innerHTML = drivers.map(driver =>
          `<div id="card">
@@ -10,8 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
           <p id="team"><b>Team: </b>${driver.team}</p>
           <p id="country"><b>Country: </b>${driver.country}</p>
           <p id="podiums"><b>Podiums: </b>${driver.podiums}</p>
-          <button type="button" class="vote-btn">Vote</button>
+          <p id="votes"><b>Votes: </b>${driver.votes}</p>
+          <button type="button" class="vote-btn" onclick="addVote('${driver._id}')">Vote</button>
         </div>
         `).join('')
     })
-})
+}
+
+function addVote(driverId) {
+    fetch(`http://localhost:3002/drivers/${driverId}/votes`, 
+        {method: 'PUT'})
+    .then(res => res.json())
+    .then(() => {
+        fetchDrivers()
+    })
+}
