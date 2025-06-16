38d944d9aed5a47c3b546b5ba21d58cb2fec39f5
Deepali Kadway <dkadway@gmail.com>
2025-06-16 12:52:57 -0600

    schema creation for users

--- models/userschema.js
+++ models/userschema.js
@@ -9,4 +9,4 @@ const userSchema = new mongoose.Schema(
     contact: {type: Number, required: true}
 })
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema, 'users')
